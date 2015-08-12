8137834eaaed00bc30e01c99b478759b07c4d288
andreasbloomquist <andreasbloomquist@gmail.com>
2015-08-12 07:45:16 -0700

    small styling

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require("express"),
   path = require("path"),
   bodyParser = require("body-parser"),
   bcrypt = require("bcrypt"),
-  session = require("express-session"),
-  db = require("./models");
+  session = require("express-session");
+  // db = require("./models");
 
 
 var views = path.join(__dirname, "views");
